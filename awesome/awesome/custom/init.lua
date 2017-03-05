129f6473ef93e9198fa563ebc989d0ccfca65066
Patrice Blin <dev@patriceblin.com>
2017-03-05 15:10:06 +0100

    awesome: add custom tags names

--- awesome/awesome/custom/init.lua
+++ awesome/awesome/custom/init.lua
@@ -1,3 +1,5 @@
+local awful = require("awful")
+
 package.loaded.custom = nil
 
 -- This is used later as the default terminal and editor to run.
@@ -11,6 +13,17 @@ editor = os.getenv("EDITOR") or "vim"
 -- However, you can use another modifier like Mod1, but it may interact with others.
 modkey = "Mod4"
 
+-- Define a tag table which hold all screen tags.
+tags = {
+  names  = { "✏️", "🌐", "💬", "🎬", "🎮" },
+  usage = { "editor", "web", "chat", "media", "games" },
+  layouts = { awful.layout.suit.max,
+              awful.layout.suit.max.fullscreen,
+              awful.layout.suit.tile,
+              awful.layout.suit.fair,
+              awful.layout.suit.tile }
+}
+
 local custom = {
     -- This is used later as the default terminal and editor to run.
     terminal = terminal,
@@ -22,7 +35,9 @@ local custom = {
     -- If you do not like this or do not have such a key,
     -- I suggest you to remap Mod4 to another key using xmodmap or other tools.
     -- However, you can use another modifier like Mod1, but it may interact with others.
-    modkey = modkey
+    modkey = modkey,
+
+    tags = tags,
 }
 
 return custom
--- awesome/awesome/rc.lua
+++ awesome/awesome/rc.lua
@@ -171,7 +171,7 @@ awful.screen.connect_for_each_screen(function(s)
     set_wallpaper(s)
 
     -- Each screen has its own tag table.
-    awful.tag({ "1", "2", "3", "4", "5", "6", "7", "8", "9" }, s, awful.layout.layouts[1])
+    awful.tag(custom.tags.names, s, custom.tags.layouts)
 
     -- Create a promptbox for each screen
     s.mypromptbox = awful.widget.prompt()
